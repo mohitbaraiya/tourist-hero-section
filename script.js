a4f365b37c7fd2b0706b2dc2d93eb09c5006bea5
mohitbaraiya <mohitbaraiya666@gmail.com>
2023-10-17 23:20:01 +0530

    WIP

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
+/**
+ * menu click handler
+ */
 const menuButton = document.getElementById("menuButton");
 const closeMenuButton = document.getElementById("closeMenu");
 const menu = document.querySelector(".menu");
 
+// close menu handler
 const closeMenu = (e) => {
   if (e.key == "Escape") {
     menu.classList.remove("active");
     window.removeEventListener("keydown", closeMenu);
   }
 };
+
+// open menu click
 menuButton.addEventListener("click", () => {
   menu.classList.add("active");
   window.addEventListener("keydown", closeMenu);
 });
+// close menu on click
 closeMenuButton.addEventListener("click", () => {
   menu.classList.remove("active");
   window.removeEventListener("keydown", closeMenu);
+});
+
+/**
+ * end menu click handler
+ */
+
+/**
+ * search form handler
+ */
+const searchForm = document.getElementById("searchForm");
+const searchFormBtn = document.getElementById("searchBtn");
+
+searchFormBtn.addEventListener("click", () => {
+  searchForm.classList.toggle("active");
 });
